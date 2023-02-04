0dca4ab23e85dff102c27517f65c1e8e5fe80d7d
Niklas Bognar <94807001+nibog001@users.noreply.github.com>
2023-02-04 21:32:09 +0100

    Update SelectedDestination.js

--- src/pages/SelectedDestination.js
+++ src/pages/SelectedDestination.js
@@ -6,7 +6,7 @@ export function SelectedDestination({ destination, onBackClick }) {
     <div class="selected-destination single-card">
       <h2>Details for {destination.destination.name}:</h2>
       {destination.journeysTo.map((journeys, idx) => (
-        <div> Depature: {new Date(journeys.legs[0].departure).toString()} </div>
+        <div key={idx}> Depature: {new Date(journeys.legs[0].departure).toString()} </div>
       ))}
       <button class="back-button" onClick={onBackClick}>Back to Results</button>
     </div>
