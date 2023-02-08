85b941be72a8091cf0c6dd26d52125279d2bd790
nibog001 <nibog001@stud.hn.de>
2023-02-08 14:28:24 +0100

    On branch main

--- src/pages/SelectedDestination.js
+++ src/pages/SelectedDestination.js
@@ -2,12 +2,14 @@
 
 
 export default function SelectedDestination({ destination, onBackClick }) {
+  if(destination=== undefined){
+    return <></>
+  }
   return (
     <div class="selected-destination single-card">
       <h2>Details for {destination.destination.name}:</h2>
-      {destination !== undefined && 
-      destination.journeysTo.map((journeys, idx) => (
-        <div key={idx}> Depature: {new Date(journeys.legs[0].departure).toString()} </div>
+      {destination.journeysTo.map((journeys, idx) => (
+          <div key={idx}> Depature: {new Date(journeys.legs[0].departure).toString()} </div>
       ))}
       <button class="back-button" onClick={onBackClick}>Back to Results</button>
     </div>
