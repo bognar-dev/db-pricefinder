78800a86b32a42ea83063036e0f17637f243971b
nibog001 <nibog001@stud.hn.de>
2023-02-08 16:28:23 +0100

    modified:   src/pages/SelectedDestination.js

--- src/pages/SelectedDestination.js
+++ src/pages/SelectedDestination.js
@@ -1,12 +1,14 @@
 
 
-
 export default function SelectedDestination({ destination, onBackClick }) {
+  if(destination=== undefined){
+    return <></>
+  }
   return (
     <div class="selected-destination single-card">
       <h2>Details for {destination.destination.name}:</h2>
       {destination.journeysTo.map((journeys, idx) => (
-        <div key={idx}> Depature: {new Date(journeys.legs[0].departure).toString()} </div>
+          <div key={idx}> Depature: {new Date(journeys.legs[0].departure).toString()} </div>
       ))}
       <button class="back-button" onClick={onBackClick}>Back to Results</button>
     </div>
